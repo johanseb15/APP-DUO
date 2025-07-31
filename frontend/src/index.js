1d047ef6e07d0ce26986dee6a3ca096f47ae3694
johanseb15 <sebasmol2010@gmail.com>
2025-07-31 16:52:24 -0300

    fix: Configure PWA for Vercel deployment and use workbox-window

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { Workbox } from 'workbox-window';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox(`${process.env.PUBLIC_URL}/service-worker.js`);
+  wb.register();
+}
